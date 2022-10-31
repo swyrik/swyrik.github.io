f41d3e90cdf1fd135a164462750a6c3df205d135
swwyrik <swwyrik@gmail.com>
2022-10-31 21:38:40 +0530

    responsive design

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var linkElements = document.querySelectorAll(".links li a");
+var linksPanel = document.querySelector(".linkspanel");
+var linksPanelBg = document.querySelector(".linkspanel-bg");
+var linkPanelElements = document.querySelectorAll(".linkspanel li a");
 var experienceElements = document.querySelectorAll("div.experience > div");
 var companyElements = document.querySelectorAll(".companies li a");
 var mainDivElements = document.querySelectorAll(".main > div");
@@ -49,6 +52,31 @@ function makeLinkElementActive(e) {
   });
 }
 
+linkPanelElements.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    makeLinkElementActive(e.target);
+  });
+});
+
+function makeLinkElementActive(e) {
+  hideLinks();
+  linkPanelElements.forEach((element) => {
+    element.classList.remove("linkActive");
+  });
+  e.classList.add("linkActive");
+  mainDivElements.forEach((element) => {
+    element.classList.remove("show");
+    element.classList.remove("show_grid");
+    if (element.classList.contains(e.parentElement.id)) {
+      if (element.classList.contains("workexperience")) {
+        element.classList.add("show_grid");
+      } else {
+        element.classList.add("show");
+      }
+    }
+  });
+}
+
 companyElements.forEach((element) => {
   element.addEventListener("click", (e) => {
     makeCompaniesElementActive(e.target);
@@ -70,4 +98,14 @@ function makeCompaniesElementActive(e) {
       expElement.classList.remove("show");
     }
   });
+}
+
+function showLinks() {
+  linksPanel.style.visibility = "visible";
+  linksPanelBg.style.display = "block";
+}
+
+function hideLinks() {
+  linksPanel.style.visibility = "hidden";
+  linksPanelBg.style.display = "none";
 }
